2a3b9cfaed90634f6c979babe777036a65e2cdbd
HoangVu@pc <vuhoang1609@gmail.com>
2021-08-27 11:14:02 +0100

    Remove dataframe from class field

--- hltv_scraper/__main__.py
+++ hltv_scraper/__main__.py
@@ -2,5 +2,5 @@ from hltv_scraper.scraper import HLTVScraper
 
 if __name__ == '__main__':
     scraper = HLTVScraper() 
-    scraper.fetch_results(max_result_count=230)
-    print(scraper.results_df)
+    df = scraper.fetch_results(max_result_count=230)
+    print(df)
--- hltv_scraper/scraper.py
+++ hltv_scraper/scraper.py
@@ -9,19 +9,22 @@ class HLTVScraper():
     
     def __init__(self, base_url="https://www.hltv.org", ret_format="pandas"):
         self.base_url = base_url
-        self.results_df = pd.DataFrame(columns=["event", "team_1", "team_2", "map", "score_1", "score_2"])
 
     def fetch_results(self, endpoint="/results", start_date=None, end_date=None, max_result_count=100):
         offset = 0
         result_rem_count = min(max_result_count - offset, 100) 
         new_row_count = 1
+        
+        df = pd.DataFrame(columns=["event", "team_1", "team_2", "map", "score_1", "score_2"])
 
         while result_rem_count > 0 and new_row_count > 0:
-            new_row_count = self.__fetch_results(endpoint, start_date, end_date, offset, result_rem_count)
-            offset += new_row_count 
+            new_row_count, df = self.__fetch_results(df, endpoint, start_date, end_date, offset, result_rem_count)
+            offset += new_row_count
             result_rem_count = min(max_result_count - offset, 100) 
+
+        return df
             
-    def __fetch_results(self, endpoint, start_date=None, end_date=None, offset=0, max_result_count=100):
+    def __fetch_results(self, df, endpoint, start_date=None, end_date=None, offset=0, max_result_count=100):
         url = urljoin(self.base_url, endpoint)
         query = {
             "startDate" : start_date,
@@ -59,9 +62,9 @@ class HLTVScraper():
             sublist_matches = [self.__parse_match(match) for match in matches] 
             sublist_matches_df = pd.DataFrame(sublist_matches)
             sublist_matches_df["date"] = datetime.strftime("%d-%m-%Y")  
-            self.results_df = self.results_df.append(sublist_matches_df, ignore_index=True)
+            df = df.append(sublist_matches_df, ignore_index=True)
 
-        return new_row_count
+        return new_row_count, df
     
 
     def __parse_match(self, html):
