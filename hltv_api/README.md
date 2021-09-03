66283bf8c0ddd3446955aa0c50d9ef03e5f7a957
HoangVu@pc <vuhoang1609@gmail.com>
2021-09-03 12:10:51 +0100

    Setup files for package distribution

---
+++ hltv_api/README.md
@@ -0,0 +1,10 @@
+# HLTV API _(unofficial)_
+
+### Installation
+
+```
+pip install hltv-api
+```
+
+### References
+- [HLTV API Python Github](https://github.com/hoangvu01/hltv_python)
---
+++ hltv_api/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hltv-api",
+    version="0.0.1",
+    description="Unofficial HLTV API to query Counter Strike historical data",
+    url="https://github.com/hoangvu01/hltv_python",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Hoang Vu",
+    author_email="vc.hoang01@gmail.com",
+    license='MIT',
+    install_requires=[
+        "lxml==4.6.3",
+        "requests==2.26.0",
+        "pandas==1.3.2" 
+    ],
+    package_dir= {"" : "hltv_api"},
+    packages=find_packages(where="hltv_api")
+)
