594dfd4cd5afa8fc693fa38a997114cf46444614
Lewis Tham <lewistham9x@gmail.com>
2024-07-14 00:27:44 +0800

    flaresolver implementation

--- src/hltv_api/client.py
+++ src/hltv_api/client.py
@@ -1,50 +1,43 @@
-import random
 import time
 from urllib.parse import urljoin
-
 import requests
-from requests import Request, Session
 
 from hltv_api.common import HLTVConfig
 from hltv_api.exceptions import HLTVRequestException
 
 
-class HLTVClient():
-    USER_AGENTS = ['hltv-api 0.1', 'python-request', 'request']
+class HLTVClient:
+    USER_AGENTS = ["hltv-api 0.1", "python-request", "request"]
 
     def __init__(self, max_retry=3):
         self.prev_response = None
-
-        if max_retry > 0:
-            self.max_retry = max_retry
+        self.max_retry = max_retry if max_retry > 0 else 3
+        self.url = "http://localhost:8191/v1"
+        self.headers = {"Content-Type": "application/json"}
 
     def get(self, url, **kwargs):
-        # TODO: Come up with algorithm to bypass rate limit
         attempt = 0
-        session = Session()
-        request = Request('GET', url=url, **kwargs)
         response = None
 
         while attempt <= self.max_retry:
-            prepped = session.prepare_request(request)
-            if self.prev_response is not None:
-                self.__handle_failed_request(prepped, only_retry_after=attempt == self.max_retry)
+            data = {"cmd": "request.get", "url": url, "maxTimeout": 60000}
+            data.update(kwargs)
 
-            response = session.send(prepped)
-            # Return response if request succeeds
+            response = requests.post(self.url, headers=self.headers, json=data)
+
             if response.ok:
                 return response
 
-            # Update timestamp and retry if required
             self.prev_response = response
-
-            # Otherwise retry
             attempt += 1
+
+            if attempt <= self.max_retry:
+                self.__handle_failed_request(only_retry_after=attempt == self.max_retry)
 
         raise HLTVRequestException(
             f"Failed to get data from HLTV after {attempt} attempt(s)",
-            response.status_code,
-            response
+            response.status_code if response else None,
+            response,
         )
 
     def search_team(self, search_term):
@@ -62,13 +55,9 @@ class HLTVClient():
         response = self.get(url, params={"term": search_term})
         return response.json()
 
-    def __handle_failed_request(self, request, only_retry_after=False):
-        if only_retry_after:
+    def __handle_failed_request(self, only_retry_after=False):
+        if only_retry_after and self.prev_response is not None:
             retry_after = self.prev_response.headers.get("Retry-After", "10")
             time.sleep(int(retry_after))
 
         # TODO: Implement API throttling mechanism
-
-        request.headers.update({
-            'User-Agent': "python-requests"
-        })
